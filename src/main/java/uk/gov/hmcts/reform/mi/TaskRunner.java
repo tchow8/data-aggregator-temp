0b36c1e239106a63ca9465443c36a07de6f6a7c8
tony.chow <tony.chow@hmcts.net>
2020-01-13 12:04:31 +0000

    Added one time run post construct

--- src/main/java/uk/gov/hmcts/reform/mi/TaskRunner.java
+++ src/main/java/uk/gov/hmcts/reform/mi/TaskRunner.java
@@ -5,25 +5,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Component
 public class TaskRunner {
 
     @Value("${fromDaysAgo}")
-    private long fromDaysAgo;
+    private String fromDaysAgo;
 
     @Value("${toDaysAgo}")
-    private long toDaysAgo;
+    private String toDaysAgo;
 
     @Autowired
     private AzureBlobStoreAccessor azureBlobStoreAccessor;
 
+    @PostConstruct
+    public void runTask() {
+        if (Objects.nonNull(fromDaysAgo)) {
+            long checkedToDaysAgo = Objects.nonNull(toDaysAgo) ? Long.parseLong(toDaysAgo) : 0l;
+            azureBlobStoreAccessor.getNotifyBlob(LocalDate.now().minusDays(Long.parseLong(fromDaysAgo)), LocalDate.now().minusDays(checkedToDaysAgo));
+            azureBlobStoreAccessor.getEightByEightBlob(LocalDate.now().minusDays(Long.parseLong(fromDaysAgo)), LocalDate.now().minusDays(checkedToDaysAgo));
+        }
+    }
+
     @Scheduled(cron = "0 0 5 * * MON")
     public void weekly() {
         System.out.println("Weekly Runner called");
-        azureBlobStoreAccessor.getNotifyBlob(LocalDate.now().minusDays(fromDaysAgo), LocalDate.now().minusDays(toDaysAgo));
-        azureBlobStoreAccessor.getEightByEightBlob(LocalDate.now().minusDays(fromDaysAgo), LocalDate.now().minusDays(toDaysAgo));
+        azureBlobStoreAccessor.getNotifyBlob(LocalDate.now().minusDays(7l), LocalDate.now());
+        azureBlobStoreAccessor.getEightByEightBlob(LocalDate.now().minusDays(7l), LocalDate.now());
     }
 
     @Scheduled(cron = "0 0 */1 ? * *")
